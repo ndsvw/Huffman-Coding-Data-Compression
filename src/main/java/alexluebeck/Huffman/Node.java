0b77d0a656f159dab4e4260cb8609dbfb44b9fea
Alex Lübeck <ndsvw@gmx.de>
2019-11-10 12:08:00 +0100

    implemented getLeft() and getRight() for Nodes

--- src/main/java/alexluebeck/Huffman/Node.java
+++ src/main/java/alexluebeck/Huffman/Node.java
@@ -5,22 +5,30 @@ public class Node {
 	private Node rightNode;
 	private int freq;
 	private String characterString;
-	
+
 	public Node(Node leftNode, Node rightNode) {
 		this.leftNode = leftNode;
 		this.rightNode = rightNode;
 		this.freq = leftNode.getFreq() + rightNode.getFreq();
 		this.characterString = null;
 	}
-	
-	public Node(int freq, String character) {
+
+	public Node(String character, int freq) {
 		this.leftNode = null;
 		this.rightNode = null;
 		this.freq = freq;
 		this.characterString = character;
 	}
-	
+
 	public int getFreq() {
 		return this.freq;
+	}
+
+	public Node getLeft() {
+		return this.leftNode;
+	}
+
+	public Node getRight() {
+		return this.rightNode;
 	}
 }
