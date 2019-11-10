4b53c4a23668f7a4ef6f62b39b6c9e802be2cc87
Alex Lübeck <ndsvw@gmx.de>
2019-11-10 11:24:15 +0100

    initial commit

---
+++ src/main/java/alexluebeck/Huffman/App.java
@@ -0,0 +1,9 @@
+package alexluebeck.Huffman;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/test/java/alexluebeck/Huffman/AppTest.java
@@ -0,0 +1,14 @@
+package alexluebeck.Huffman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
