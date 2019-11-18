6f25797ff16b398848e2f6f62fecc5c240267f30
Alex Lübeck <ndsvw@gmx.de>
2019-11-18 11:14:53 +0100

    added a larger test case (10 elements)

--- src/test/java/alexluebeck/Huffman/HuffmanTest.java
+++ src/test/java/alexluebeck/Huffman/HuffmanTest.java
@@ -50,4 +50,31 @@ public class HuffmanTest {
 		assertEquals(encodingTable.get("a").length(), 2);
 	}
 
+	@Test
+	public void test10Elements() {
+		Node n1 = new Node("a", 74);
+		Node n2 = new Node("b", 46);
+		Node n3 = new Node("c", 25);
+		Node n4 = new Node("d", 48);
+		Node n5 = new Node("e", 13);
+		Node n6 = new Node("f", 37);
+		Node n7 = new Node("g", 97);
+		Node n8 = new Node("h", 77);
+		Node n9 = new Node("i", 45);
+		Node n10 = new Node("j", 96);
+		PriorityQueue<Node> pq = new PriorityQueue<Node>(Arrays.asList(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10));
+		Huffman huffman = new Huffman(pq);
+		HashMap<String, String> encodingTable = huffman.createEncodingTable();
+		assertEquals(encodingTable.get("a").length(), 3);
+		assertEquals(encodingTable.get("b").length(), 4);
+		assertEquals(encodingTable.get("c").length(), 5);
+		assertEquals(encodingTable.get("d").length(), 3);
+		assertEquals(encodingTable.get("e").length(), 5);
+		assertEquals(encodingTable.get("f").length(), 4);
+		assertEquals(encodingTable.get("g").length(), 2);
+		assertEquals(encodingTable.get("h").length(), 3);
+		assertEquals(encodingTable.get("i").length(), 4);
+		assertEquals(encodingTable.get("j").length(), 3);
+	}
+
 }
